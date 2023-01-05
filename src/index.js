b9c2f1d15bf424cd8566ad0c4251c170e3e922b7
Western8 <westernchrome@gmail.com>
2023-01-05 19:11:12 +0300

    feat: add function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function reverse (n) {
-  
+
+    let res = '';
+
+    res = (n + '').split('');
+    res = res.reverse();
+    res = res.join('');
+    res = parseInt(res);
+
+    return res;
 }
+
+
+/*
+const n = -12345;
+console.log(n);
+const res = module.exports(n);
+console.log(res);
+*/
